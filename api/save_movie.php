b30c626d4b803b9cd15532a538ce8810472e0426
reg <reg8740223@gmail.com>
2025-01-10 10:35:13 +0800

    完成編輯電影功能

--- api/save_movie.php
+++ api/save_movie.php
@@ -15,9 +15,11 @@ $_POST['ondate']=$_POST['year']."-".$_POST['month']."-".$_POST['day'];
 unset($_POST['year'],$_POST['month'],$_POST['day']);
 
 
+if(!isset($_POST['id'])){
 
-$_POST['sh']=1;
-$_POST['rank']=$Movie->max('id')+1;
+    $_POST['sh']=1;
+    $_POST['rank']=$Movie->max('id')+1;
+}
 
 
 $Movie->save($_POST);
